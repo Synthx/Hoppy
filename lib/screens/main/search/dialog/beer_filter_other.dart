fa8b96cefd8e6183a66268f43201ae7baf9142d6
Remi <taniel.remi@gmail.com>
2021-04-04 13:13:39 +0200

    :sparkles: : add others filters

---
+++ lib/screens/main/search/dialog/beer_filter_other.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hoppy/core/core.dart';
+import 'package:hoppy/generated/l10n.dart';
+import 'package:hoppy/store/store.dart';
+
+class BeerFilterOther extends StatelessWidget {
+  void _havePictureChanged(BuildContext context, bool selected) {
+    context.read<SearchCubit>().setHavePictureFilter(selected ? true : null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(kDefaultPadding),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            Localization.of(context).beer_filter_others,
+            style: Theme.of(context).textTheme.headline6!.copyWith(
+                  fontSize: 18,
+                ),
+          ),
+          const SizedBox(height: 20),
+          BlocBuilder<SearchCubit, SearchState>(
+            buildWhen: (prev, curr) =>
+                prev.filter.havePicture != curr.filter.havePicture,
+            builder: (context, state) {
+              return ListTile(
+                title: Text(
+                  Localization.of(context).beer_filter_others_picture_title,
+                ),
+                subtitle: Text(
+                  Localization.of(context).beer_filter_others_picture_content,
+                ),
+                contentPadding: const EdgeInsets.all(0),
+                trailing: CupertinoSwitch(
+                  value: state.filter.havePicture ?? false,
+                  onChanged: (value) => _havePictureChanged(context, value),
+                  trackColor: Theme.of(context).backgroundColor,
+                  activeColor: Theme.of(context).primaryColor,
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/main/search/dialog/search_beer_filter_dialog.dart
+++ lib/screens/main/search/dialog/search_beer_filter_dialog.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hoppy/core/core.dart';
+import 'package:hoppy/generated/l10n.dart';
+import 'package:hoppy/store/store.dart';
+
+import 'beer_filter_color.dart';
+import 'beer_filter_country.dart';
+import 'beer_filter_degree.dart';
+import 'beer_filter_other.dart';
+import 'beer_filter_style.dart';
 
 class SearchBeerFilterDialog extends StatefulWidget {
   static MaterialPageRoute route() => MaterialPageRoute(
@@ -12,33 +22,72 @@ class SearchBeerFilterDialog extends StatefulWidget {
 
 class _SearchBeerFilterDialogState extends State<SearchBeerFilterDialog> {
   void _closeDialog() {
-    Navigator.pop(context);
+    context.pop();
+  }
+
+  void _search() {
+    context.pop(true);
+  }
+
+  void _resetFilter() {
+    context.read<SearchCubit>().resetFilter();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Filtre'),
+        title: Text(
+          Localization.of(context).filter,
+        ),
         leading: IconButton(
           onPressed: () => _closeDialog(),
           icon: const Icon(Icons.clear),
         ),
+        actions: [
+          TextButton(
+            onPressed: () => _resetFilter(),
+            style: TextButton.styleFrom(
+              primary: Theme.of(context).textTheme.bodyText1!.color,
+            ),
+            child: Text(
+              Localization.of(context).reset,
+            ),
+          ),
+        ],
       ),
-      body: ListView(
-        physics: const BouncingScrollPhysics(),
-        children: [],
+      body: Scrollbar(
+        child: ListView(
+          physics: const BouncingScrollPhysics(),
+          padding: const EdgeInsets.symmetric(
+            vertical: kDefaultPadding,
+          ),
+          children: [
+            BeerFilterColor(),
+            const SizedBox(height: 10),
+            BeerFilterStyle(),
+            const SizedBox(height: 10),
+            BeerFilterDegree(),
+            const SizedBox(height: 10),
+            BeerFilterCountry(),
+            const SizedBox(height: 10),
+            BeerFilterOther(),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomAppBar(
         child: Container(
-          height: 60,
+          height: kFooterButtonHeight,
+          width: double.infinity,
           margin: const EdgeInsets.symmetric(
-            horizontal: 20,
+            horizontal: kDefaultPadding,
             vertical: 10,
           ),
           child: ElevatedButton(
-            onPressed: () => _closeDialog(),
-            child: const Text('Rechercher'),
+            onPressed: () => _search(),
+            child: Text(
+              Localization.of(context).search,
+            ),
           ),
         ),
       ),
